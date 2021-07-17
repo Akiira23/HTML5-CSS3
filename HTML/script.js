7e09e1185b201d128c0e6ebde414cce82893ce06
Andre Akira <aah950@hotmail.com>
2021-07-17 16:24:36 -0300

    javascript

---
+++ HTML/script.js
@@ -0,0 +1,7 @@
+function myFunction() 
+{
+    if (document.getElementById("demo").innerHTML == "Paragraph changed.")
+        document.getElementById("demo").innerHTML = "A Paragraph."
+    else
+        document.getElementById("demo").innerHTML = "Paragraph changed."
+}
